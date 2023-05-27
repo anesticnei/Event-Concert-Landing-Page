7fddad3eb1c91c9cc308052ec3949bb818c97267
Damian Pabian <anesticnei@gmail.com>
2023-05-27 18:13:06 +0200

    Refactoring, editing position of the modal

--- src/index.ts
+++ src/index.ts
@@ -214,6 +214,14 @@ function renderArtist(favouriteArtist: Artist) {
     avatar_img: string;
     background_img: string;
     about_artist: string;
+    music:{
+      1: string;
+      2: string;
+      3: string;
+      4: string;
+      5: string;
+      6: string;
+    }
   }
   
   const favouriteArtist: Artist[] = [
@@ -222,36 +230,85 @@ function renderArtist(favouriteArtist: Artist) {
       avatar_img: "https://images.unsplash.com/photo-1550560888-7bd784b8ebec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
       background_img: "https://images.unsplash.com/photo-1550560888-7bd784b8ebec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
       about_artist: "",
+      music:{
+        1: "string",
+        2: "string",
+        3: "string",
+        4: "string",
+        5: "string",
+        6: "string",
+      }
     },
     {
       name: "Pedro Moreno",
       avatar_img: "https://images.unsplash.com/photo-1576409372120-ffc3d5c260e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80",
       background_img: "https://images.unsplash.com/photo-1576409372120-ffc3d5c260e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80",
       about_artist: "",
+      music:{
+        1: "string",
+        2: "string",
+        3: "string",
+        4: "string",
+        5: "string",
+        6: "string",
+      }
     },
     {
       name: "Xenno",
       avatar_img: "https://images.unsplash.com/photo-1575426158836-0be172e108ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80",
       background_img: "https://images.unsplash.com/photo-1575426158836-0be172e108ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80",
       about_artist: "",
+      music:{
+        1: "string",
+        2: "string",
+        3: "string",
+        4: "string",
+        5: "string",
+        6: "string",
+      }
     },
     {
       name: "Anomaly",
       avatar_img: "https://images.unsplash.com/photo-1599839619722-39751411ea63?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1588&q=80",
       background_img: "https://images.unsplash.com/photo-1599839619722-39751411ea63?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1588&q=80",
       about_artist: "",
+      music:{
+        1: "string",
+        2: "string",
+        3: "string",
+        4: "string",
+        5: "string",
+        6: "string",
+      }
     },
     {
       name: "Christian Terson",
       avatar_img: "https://images.unsplash.com/photo-1575285113814-f770cb8c796e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
       background_img: "https://images.unsplash.com/photo-1575285113814-f770cb8c796e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
       about_artist: "",
+      music:{
+        1: "string",
+        2: "string",
+        3: "string",
+        4: "string",
+        5: "string",
+        6: "string",
+      }
     },
     {
       name: "Jon Altes",
       avatar_img: "https://images.unsplash.com/photo-1431308305062-f218b6fe520a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
       background_img: "https://images.unsplash.com/photo-1431308305062-f218b6fe520a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
       about_artist: "",
+      music:{
+        1: "string",
+        2: "string",
+        3: "string",
+        4: "string",
+        5: "string",
+        6: "string",
+      }
+
     },
   ];
   
@@ -261,26 +318,47 @@ function renderArtist(favouriteArtist: Artist) {
 
   
   function renderArtistModal(artist: Artist) {
-    const artistModalContainer = document.querySelector('.artis-module-container') as HTMLDivElement;
+    // const artistModalContainer = document.querySelector('.artist-module-container') as HTMLDivElement;
     // const modalWrapper = document.querySelector('.artist-module-wrapper') as HTMLDivElement;
-  
+    const artistBottomWrapper = document.querySelector('.artist-bottom') as HTMLDivElement;
+    const artistModalContainer = document.createElement('div') as HTMLDivElement;
+    artistModalContainer.className = 'artist-module-container';
+    // const artistModalContainer = document.querySelector('.artist-module-container') as HTMLDivElement;
+
+
+
+    const backgroundModal = document.createElement('div') as HTMLDivElement;
+    backgroundModal.className = 'background-modal';
+
+    artistModalContainer.appendChild(backgroundModal);
+    artistBottomWrapper.appendChild(artistModalContainer)
+
+
+
+
     if (artistModalContainer !== null) {
-    //   const backgroundModal = document.createElement('div');
-    //   backgroundModal.className = 'background-modal';
-      artistModalContainer.style.display = 'block';
+      // artistModalContainer.style.display = 'block';
   
       const artistModal = document.createElement('div');
       artistModal.classList.add('artist-module-wrapper');
+      // artistModal.style.backgroundImage = `url(${artist.background_img})`;
+
+      const artistBox = document.createElement('div');
+      artistBox.classList.add('artist-box');
+      artistBox.style.backgroundImage = `url(${artist.background_img})`;
   
     //   artistModalContainer.appendChild(backgroundModal);
-      artistModalContainer.appendChild(artistModal);
+    artistModalContainer.appendChild(artistModal);
+    artistModal.appendChild(artistBox);
   
-      artistModal.innerHTML = `
+    artistBox.innerHTML = `
         <div class="previewModal--avatar wh-100">
-          <img class="previewModal--avatar-img wh-100 object-cover" src="${artist.background_img}" alt="">
+
         </div>
         <div class="previewModal--close wh-100 cursor-pointer">
-          X
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 20.953 20.953">
+        <path id="close" d="M28.477,9.619l-2.1-2.1L18,15.9,9.619,7.523l-2.1,2.1L15.9,18,7.523,26.381l2.1,2.1L18,20.1l8.381,8.381,2.1-2.1L20.1,18Z" transform="translate(-7.523 -7.523)" fill="#fff"/>
+      </svg>      
         </div>
         <div class="previewModal--info wh-100 padd-1rem flex flex-direction-column">
           <h1 class="artist-title mar">${artist.name}</h1>
@@ -328,23 +406,24 @@ function renderArtist(favouriteArtist: Artist) {
         e.stopPropagation();
 
         return false;
-    }
+      }
+      // document.body.style.overflow = "hidden"
   const closeModalBtn = document.querySelectorAll('.previewModal--close');
-  const backgroundModal = document.querySelector('.background-modal') as HTMLDivElement;
+  // const backgroundModal = document.querySelector('.background-modal') as HTMLDivElement;
   backgroundModal.style.display = "block"
   closeModalBtn.forEach(btn => 
     btn.addEventListener("click", () =>{
       document.body.removeEventListener('wheel', preventScroll);
           const artistModal = document.querySelector('.artist-module-wrapper') as HTMLDivElement;
           if(artistModalContainer)
-          artistModal.remove()
+          artistModalContainer.remove()
           artistModalContainer.style.display = 'none';
           backgroundModal.style.display = "none"
         }))
             backgroundModal?.addEventListener('click', () => {
               document.body.removeEventListener('wheel', preventScroll);
                 const artistModal = document.querySelector('.artist-module-wrapper') as HTMLDivElement;
-                  artistModal.remove()
+                artistModalContainer.remove()
                     if(artistModalContainer)
                     artistModalContainer.style.display = 'none';
                     backgroundModal.style.display = "none"
